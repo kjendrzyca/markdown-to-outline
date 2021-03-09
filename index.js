a0d915dfc5e1ee8bf2f0e0df4d006c48bf2d4828
kjendrzyca <kjendrzyca@users.noreply.github.com>
2021-03-09 21:51:31 +0100

    Fix path resolution in the script

--- index.js
+++ index.js
@@ -5,12 +5,11 @@ const toOutline = require('./mapToOutline')
 
 const { argv } = process
 
-const dirname = path.dirname(argv[1])
-const filepath = path.join(dirname, argv[2])
+const filepath = path.resolve(process.cwd(), argv[2])
 
 const mdFile = fs.readFileSync(filepath)
 
 const outlineFile = toOutline(toJson(mdFile))
-const outputPath = path.join(dirname, `${argv[2]}.output`)
+const outputPath = path.resolve(process.cwd(), `${argv[2]}.output`)
 
 fs.writeFileSync(outputPath, outlineFile)
