605952af22c244bd90fcfd7e210064317ed40751
kjendrzyca <kjendrzyca@users.noreply.github.com>
2021-03-12 09:37:30 +0100

    Add item list note multiline mapping

--- mapToJson.js
+++ mapToJson.js
@@ -8,7 +8,7 @@ module.exports = (markdownDocument) => {
   const allElements = tree.children
 
   // console.log(allElements)
-  // console.log(JSON.stringify(tree, null, 2))
+  // console.log(JSON.stringify(allElements, null, 2))
 
   // THIS DOESN'T WORK, because I would indent h1, when indenting stuff between two h2
   // find indexes between headings with `depth=1` => [0, 10, 20]
@@ -63,7 +63,6 @@ const mapStuff = (headingIndexes, elements, depth) => {
   const elementsAfterFirstIndex = headingIndexes.map((index, indexIterator) => {
     const isFirstGroupIndex = index === headingIndexes[0]
     const isLastGroupIndex = index === headingIndexes[headingIndexes.length - 1]
-    // TODO: hardcoded 0 shouldn't be here
     const isInBetweenGroupIndex = (
       index > headingIndexes[0] && index < headingIndexes[headingIndexes.length - 1]
     )
@@ -139,17 +138,13 @@ const sanitizeElement = (element) => {
   if (element.type === 'list') {
     return element.children.map((child) => ({
       ...elementFactory(child),
-      value: child.children[0].children[0].value,
+      value: mapParagraph(child.children[0]),
     }))
   }
 
-  const paragraph = element.children.map(
-    (child) => (child.value ? child.value : child.children[0].value),
-  ).join('')
-
   return [{
     ...elementFactory(element),
-    value: paragraph,
+    value: mapParagraph(element),
   }]
 }
 
@@ -158,57 +153,13 @@ const elementFactory = (element) => ({
   ...(element.depth ? { depth: element.depth } : {}),
 })
 
+const mapParagraph = (element) => element.children.map(
+  (child) => (child.value ? child.value : child.children[0].value),
+).join('')
+
 // TODO: clean up
 const sanitizeHeader = (element) => ({
   type: element.type,
   ...(element.depth ? { depth: element.depth } : {}),
   value: element.children ? element.children[0].value : element.value,
 })
-
-// =========================
-
-// const finalResult = levelOneIndexes.map((index) => {
-//   const isFirstGroupIndex = index === 0
-//   const isLastGroupIndex = index === levelOneIndexes[levelOneIndexes.length - 1]
-
-//   console.log({ isLastGroupIndex, index, checked: levelOneIndexes[levelOneIndexes.length - 1] })
-
-//   let group
-//   if (isFirstGroupIndex) {
-//     const secondHeaderIndex = levelOneIndexes[1]
-//     group = allElements.reduce((result, current, currentIndex) => {
-//       if (currentIndex > 0 && currentIndex < secondHeaderIndex) {
-//         result.push(sanitizeElement(current))
-//       }
-
-//       return result
-//     }, [])
-//   }
-
-//   if (isLastGroupIndex) {
-//     group = allElements.reduce((result, current, currentIndex) => {
-//       if (currentIndex > index) {
-//         result.push(sanitizeElement(current))
-//       }
-
-//       return result
-//     }, [])
-//   }
-
-//   return { groupFor: sanitizeElement(allElements[index]), group }
-// })
-
-// console.log(finalResult)
-
-// const reducedTree = tree.reduce((prev, current, currentIndex) => {
-
-// }, {});
-
-// console.log(reducedTree);
-
-// if (isLastGroupIndex) {
-//   console.log({ currentIndex, index })
-//   if (currentIndex > index) {
-//     result.push(sanitizeElement(current))
-//   }
-// }
--- testData/6-note-input.md
+++ testData/6-note-input.md
@@ -5,6 +5,7 @@ Paragraph 1
 
 Rule1
 **Note:** !!!
+Multiline
 
 ### H3
 **Note:** h3 note
@@ -16,3 +17,10 @@ H3 Paragraph with note
 
 Rule2
 **Note:** aaa
+
+1. Equally less relevant for the digital version are those links that indicate the note to which the current note is a follow- up and those links that indicate the note that follows on the current note.
+**Note:** !!!
+
+2. The most common form of reference is plain note- to- note links. They have no function other than indicating a relevant connection between two individual notes.
+**Note:** !!!
+Multiline
